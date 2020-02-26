356a89fb4394db0b109219c4764254a4f9e2ca81
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-02-26 14:08:21 +0100

    Add files via upload

---
+++ indexView.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Billet simple pour l'Alaska</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/png" href="pics/logo.png"> <! -- a faire -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/f42ba57ba1.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <link href="style.css" rel="stylesheet"/>
+    </head>
+        
+    <body>
+        <div>
+            <header class="container">
+                <nav>
+                    <ul class="nav">
+                        <li class="nav-item"><a class="nav-link" href="">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">A propos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">Me contacter</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <section class="container-fluid" id="section_img">
+                <figure>
+                    <img class="img-fluid" src="pics/1.jpg" alt="Alaska's lake"/>
+                    <figcaption>Billet simple pour l'Alaska<br>
+                    de Jean Forteroche</figcaption>
+                </figure>
+            </section>
+
+            <article>
+            </article>
+
+            <article>
+            </article>
+
+            <article>
+            </article>
+
+            <footer class="container navbar fixed-bottom">
+                <div class="row">
+                    <ul id="social_networks" class="d-flex col-2 list-unstyled">
+                        <li><a href=""><i class="fab fa-twitter fa-2x" class="img-circle"></i></a></li>
+                        <li><a href=""><i class="fab fa-facebook fa-2x" class="img-circle"></i></a></li>
+                        <li><a href=""><i class="fab fa-instagram fa-2x" class="img-circle"></i></a></li>
+                    </ul>
+                    <a class="col-3 text-center" href="">Mentions légales</a>
+                    <a class="col-3 text-center" href="login.php">Connection (admin)</a>
+                    <p class="col-4 footer-copyright text-center">Copyright © All rights reserved. Billet simple pour l'Alaska 2020</p>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
