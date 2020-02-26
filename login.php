e33ee5d41f95a863de593ca99cf47a6264128901
lserrano63 <toushalo@hotmail.fr>
2020-02-26 16:27:10 +0100

    connection utilisateur

--- login.php
+++ login.php
@@ -13,24 +13,39 @@
         <link href="style.css" rel="stylesheet"/>
     </head>
 
-    <section id="login" class="container">
-		<div class="card card-container">
-			<h2 class="card-header">Connexion</h2>
-			<div class="card-body">
-				<div class="login-form">
-					<form action="" method="post">
-						<div class="form-group">
-							<label for="pseudo">Pseudo :</label>
-							<input name="name" type="text" id="name" class="form-control" required/>
-						</div>
-						<div class="form-group">
-							<label for="password">Mot de Passe :</label>
-							<input type="password" name="password" id="password" class="form-control" required/>
-						</div>
-						<input type="submit" name="connection" class="btn btn-primary" value="Connexion"/>
-					</form>
-					
+	<body>	
+	<?php 
+	if (isset($_POST["name"]) && strtolower($_POST["name"]) == "jean" ){
+		if (isset($_POST["password"]) &&  password_verify($_POST["password"], '$2y$10$sIGNT2Wp9ml93ribg8qRTONt/vPJx7thWWExq8gj1zUhsyVZpXYVi')){
+			
+			echo "Bonjour Jean!";
+		} else {
+			echo 'pass non valide';
+
+		}
+	}
+	?>
+	
+		<section id="login" class="container">
+			<div class="card card-container">
+				<h2 class="card-header">Connexion</h2>
+				<div class="card-body">
+					<div class="login-form">
+						<form action="login.php" method="post">
+							<div class="form-group">
+								<label for="pseudo">Pseudo :</label>
+								<input name="name" type="text" id="name" class="form-control" required/>
+							</div>
+							<div class="form-group">
+								<label for="password">Mot de Passe :</label>
+								<input type="password" name="password" id="password" class="form-control" required/>
+							</div>
+							<input type="submit" name="connection" class="btn btn-primary" value="Connexion"/>
+						</form>
+
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
+	</body>
+</html>
--- model.php
+++ model.php
@@ -3,7 +3,7 @@
 {
     try
     {
-        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'bjhv jh');
     }
     catch(Exception $e)
     {
