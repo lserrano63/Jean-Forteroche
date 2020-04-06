04badbe71b9d449892ac2db1e1e58fc3d63d4dd5
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-04-06 04:34:06 +0200

    Ajout acces admin a propos

--- Views/adminView.php
+++ Views/adminView.php
@@ -5,6 +5,7 @@
     <ul class="container list-unstyled d-flex justify-content-center flex-column">
         <li class="btn btn-secondary mt-3"><a class="text-light" href="?action=postCreation">Nouveau post</a></li>
         <li class="btn btn-secondary mt-3"><a class="text-light" href="?action=adminPost">Administration des posts</a></li>
+        <li class="btn btn-secondary mt-3"><a class="text-light" href="?action=adminBio">Administration de la biographie</a></li>
         <li class="btn btn-secondary mt-3"><a class="text-light" href="?action=viewReportedComments">Commentaires Signalés</a></li>
     </ul>
 </section>
