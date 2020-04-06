6f0a5062f0b78495932f86411e4aaf0c032e1afd
lserrano63 <55395444+lserrano63@users.noreply.github.com>
2020-04-06 04:37:42 +0200

    tinymce pour modifier a propos via admin

--- scripts/tiny.js
+++ scripts/tiny.js
@@ -1,5 +1,21 @@
 tinymce.init({
-    selector: '#post',
+    selector: '#post ',
+    setup:function(editor){
+        editor.on('change', function(){
+            tinymce.triggerSave();
+        })
+    }
+});
+tinymce.init({
+    selector: '#bio ',
+    setup:function(editor){
+        editor.on('change', function(){
+            tinymce.triggerSave();
+        })
+    }
+});
+tinymce.init({
+    selector: '#contact ',
     setup:function(editor){
         editor.on('change', function(){
             tinymce.triggerSave();
